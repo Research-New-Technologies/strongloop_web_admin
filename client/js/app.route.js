4b70d1b06966d1eb53e15a773ea8375d1351cb8e
Paulus_H <paulus.herdiyatma@mitrais.com>
2015-12-22 12:13:52 +0800

    add AdminLTE template

--- client/js/app.route.js
+++ client/js/app.route.js
@@ -1,1 +1,11 @@
-angular.module('app');
+angular.module('app')
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('employee', {
+        url: '',
+        templateUrl: 'modules/employee/views/employee.html',
+        controller: 'EmployeeController'
+      });
+
+    $urlRouterProvider.otherwise('employee');
+  }]);
