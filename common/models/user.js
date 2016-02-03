a1ac410632fca3685abd47fb273bf2b5c6525a52
Paulus_H <paulus.herdiyatma@mitrais.com>
2016-02-03 16:44:21 +0800

    add comment

--- common/models/user.js
+++ common/models/user.js
@@ -2,15 +2,27 @@ module.exports = function (user) {
     var loopBack = require('../../node_modules/loopback/lib/loopback');
     var app = require('../../server/server');
     
-    
     //check whether the request is from Admin or unauthenticated member.
     user.beforeRemote('create', function (context, userInstance, next) {
+        
+        //define object model
+        var accessToken = app.models.AccessToken;
+        var role = app.models.Role;
+        var roleMapping = app.models.RoleMapping;
+        
+        //add created date and last updated  with current date to the request body.
         context.req.body.createdDate = new Date();
         context.req.body.lastUpdated = new Date();
+        
+        //when request body doesn't have profilePicture, then set it to default profile picture
         if (typeof (context.req.body.profilePicture) == 'undefined') {
             context.req.body.profilePicture = 'storages/missing/missing-image.png';
         }
+        
+        //create a new error variable and it will used later
         var error = new Error();
+        
+        //when there is no username property in the request body then return BAD REQUEST error
         if (typeof (context.req.body.username) == 'undefined' || context.req.body.username == '') {
             error.name = 'BAD_REQUEST'
             error.status = 400;
@@ -18,6 +30,8 @@ module.exports = function (user) {
             error.code = 'USERNAME_IS_EMPTY';
             return next(error)
         }
+        
+        //when password and confirm password don't match then return BAD request error
         else if (context.req.body.password != context.req.body.confirmPassword) {
             error.name = 'BAD_REQUEST'
             error.status = 400;
@@ -25,29 +39,63 @@ module.exports = function (user) {
             error.code = 'USERNAME_IS_EMPTY';
             return next(error)
         }
+        
+        //when request body is acceptable
         else {
+            
+            //check whether there is a user with same email with request body email 
             user.find({ where: { 'email': context.req.body.email } }, function (err, userResults) {
+                
+                //if there is a user with same email
                 if (userResults.length == 1) {
+                    
+                    //check whether email already verified or not
                     if (!userResults[0].emailVerified) {
+                        
+                        //when it doesn't verified  
+                        //retrieve the created time of the current user
                         var createdTime = userResults[0].createdDate.getTime();
+                        
+                        //get current time
                         var currentTime = new Date().getTime();
+                        
+                        //get the difference between current time and created time and convert it into minutes
                         var time_range_in_minutes = (currentTime - createdTime) / 60000;
+                        
+                        //check whether the difference time is bigger than determined interval in config.json
                         if (time_range_in_minutes >= user.app.settings.repeatedSignupInterval) {
+                            
+                            //when the difference is bigger, then do destroy user
                             user.destroyById(userResults[0].__data.id, function (err, res) {
+                                
+                                //after destroy user, do find roleMapping object based on destroyed user id
                                 app.models.RoleMapping.find({ where: { 'userId': userResults[0].__data.id } }, function (err, roleMappingResults) {
+                                    
+                                    //when it has roleMapping, then looping into every items
                                     roleMappingResults.forEach(function (roleMappingObject) {
+                                        
+                                        //remove roleMapping object from database
                                         roleMappingObject.remove();
                                     })
+                                    
+                                    //delete current user & current role mapping has done, then go to default create user
                                     next();
                                 });
                             })
                         }
-
+                        
+                        //when the diffrence time is smaller than the determined interval
                         else {
+                            
+                            //calculate difference between determined interval with accumulation time from the created date of current user with the created date of the new user
                             var interval = (Math.round(user.app.settings.repeatedSignupInterval - time_range_in_minutes));
+                            
+                            //when interval equal zero, then make it as 1
                             if (interval == 0) {
                                 interval = 1;
                             }
+                            
+                            //return an error
                             var error = new Error();
                             error.name = '412'
                             error.status = 400;
@@ -56,18 +104,19 @@ module.exports = function (user) {
                             return next(error)
                         }
                     }
+                    
+                    //if the current user already verified their email address, then go to next
                     else {
                         next();
                     }
                 }
+
+                //if there is no current user with same email address
                 else {
-                    //define object model
-                    var accessToken = app.models.AccessToken;
-                    var role = app.models.Role;
-                    var roleMapping = app.models.RoleMapping;
-        
+       
                     //check whether access token is valid or not
                     accessToken.findForRequest(context.req, {}, function (aux, accessTokenResult) {
+                        
                         //request is from unauthenticated member
                         if (typeof (accessTokenResult) == 'undefined') {
                             next();
@@ -78,10 +127,13 @@ module.exports = function (user) {
                             //retrive role admin object
                             role.find({ where: { name: 'admin' } }, function (err, roleResults) {
                                 var roleAdmin = roleResults[0];
+                                
                                 //check whether there is role mapping from memberId and role admin id
                                 roleMapping.find({ where: { principalId: context.req.accessToken.userId, roleId: roleAdmin.id } }, function (err, roleMappingResults) {
                                     //if the request from admin user
                                     if (roleMappingResults.length > 0) {
+                                        
+                                        //get request body and add lastUpdated & emailVerified property
                                         var userRequest = context.req.body;
                                         userRequest.createdDate = new Date();
                                         user.lastUpdated = new Date();
@@ -101,6 +153,8 @@ module.exports = function (user) {
                                 
                                             //successfully create a new admin
                                             else {
+                                                
+                                                //create roleMapping from a new admin user
                                                 roleAdmin.principals.create({
                                                     principalType: roleMapping.USER,
                                                     principalId: userResult.id
@@ -114,17 +168,14 @@ module.exports = function (user) {
                                         });
                                     }
                         
-                                    //role mapping with adminId and roleId Admin not found
+                                    //roleMapping with adminId and roleId Admin not found
                                     else {
                                         next();
                                     }
                                 })
                             })
                         }
-                    },
-                        function (error) {
-                            console.log(error)
-                        })
+                    })
                 }
             })
         }
@@ -145,6 +196,8 @@ module.exports = function (user) {
                     redirect: encodeURIComponent('/#/confirmation'),
                     user: userInstance,
                 };
+                
+                //send an email to the new member user
                 userInstance.verify(options, function (err, response) {
                     if (err) return next(err);
                     next();
@@ -199,7 +252,7 @@ module.exports = function (user) {
     //when user has successfully logged, then add role name & profile picture
     user.afterRemote('login', function (context, userInstance, next) {
         var roleMapping = app.models.RoleMapping;
-        roleMapping.find({ include: { relation: 'role' }, where: { principalId: context.result.__data.userId } }, function (err, roleMappingResults) {
+        user.find({ include: { relation: 'role' }, where: { principalId: context.result.__data.userId } }, function (err, roleMappingResults) {
             if (roleMappingResults.length > 0) {
                 var host = (user.app && user.app.settings.host);
                 var port = (user.app && user.app.settings.port);
@@ -217,12 +270,15 @@ module.exports = function (user) {
         next();
     });
 
-    //reset the user's pasword
+    //reset the user's password
     user.beforeRemote('resetPassword', function (context, userInstance, next) {
+        var accessToken = app.models.AccessToken;
+        //check whether the request body has pssword property
         if (context.req.body.password) {
+            //check whether there is accessToken header or not
             if (!context.req.headers.accessToken) return context.res.sendStatus(401);
       
-            //verify passwords match
+            //verify passwords match then return error if it doesn't match
             if (!context.req.body.password ||
                 !context.req.body.confirmPassword ||
                 context.req.body.password !== context.req.body.confirmPassword) {
@@ -235,14 +291,48 @@ module.exports = function (user) {
                 return next(error)
             }
 
-            user.findById(context.req.body.id, function (err, userResult) {
-                if (err) return context.res.sendStatus(404);
-                userResult.updateAttribute('password', context.req.body.password, function (err, userUpdateResult) {
-                    if (err) return context.res.sendStatus(404);
-                    console.log('> password reset processed successfully');
-                    return context.res.sendStatus(202);
-                });
-            });
+            else {
+                //check whether access token is valid or not
+                accessToken.findById(context.req.headers.accessToken, function (err, accessTokenResult) {
+                    
+                    //if the access token already saved in database
+                    if (accessTokenResult != null) {
+                        
+                        //validate access token
+                        accessTokenResult.validate(function (err, isValid) {
+                            
+                            //when access token is valid
+                            if (isValid) {
+                                
+                                //find user by request body user Id
+                                user.findById(accessTokenResult.userId, function (err, userResult) {
+                                    
+                                    //return not found when user with the id not found in database
+                                    if (err) return context.res.sendStatus(404);
+                                    
+                                    //update user password
+                                    userResult.updateAttribute('password', context.req.body.password, function (err, userUpdateResult) {
+                                        if (err) return context.res.sendStatus(404);
+                                        console.log('> password reset processed successfully');
+                                        return context.res.sendStatus(202);
+                                    });
+                                });
+
+                            }
+                            
+                            //when access token is invalid
+                            else {
+                                return context.res.sendStatus(401);
+                            }
+                        })
+                    }
+                    
+                    //when access token is not available in the database
+                    else {
+                        return context.res.sendStatus(401);
+                    }
+                })
+            }
         }
         else {
             next();
@@ -253,21 +343,31 @@ module.exports = function (user) {
     
     //remote method - find users
     user.beforeRemote('find', function (context, userInstance, next) {
+        
+        //get filter query from the request
         var queryFilter = JSON.parse(context.req.query.filter);
+       
         var roleMapping = app.models.RoleMapping;
         var results = [];
+        
         var host = (user.app && user.app.settings.host);
         var port = (user.app && user.app.settings.port);
+        
         //check if the query filter is roleId, then prevent to next, do query and return the results
         if (queryFilter.order == "roleId DESC" || queryFilter.order == "roleId ASC") {
             //use "include ['user','role'] to add user and role object into roleMapping"
             roleMapping.find({ limit: queryFilter.limit, skip: queryFilter.skip, order: queryFilter.order, include: ['user', 'role'] }, function (err, roleMappingResults) {
                 roleMappingResults.forEach(function (element, index, array) {
                     var user = element.__data.user.__data;
+                    
+                    //add role name
                     user.roleName = element.__data.role.__data.name;
+                    
+                    //add profile host and port to profile picture url
                     user.profilePicture = 'http://' + host + ':' + port + '/' + user.profilePicture
                     results.push(user)
                     
+                    //when the looping has done
                     if (index == roleMappingResults.length - 1) {
                         return context.res.status(200).send(results);
                     }
@@ -285,32 +385,45 @@ module.exports = function (user) {
         var roleMapping = app.models.RoleMapping;
         var host = (user.app && user.app.settings.host);
         var port = (user.app && user.app.settings.port);
+        
+        //looping for every item of results
         context.result.forEach(function (result, index, array) {
+            
+            //add host and port to the profile picture url
             result.__data.profilePicture = 'http://' + host + ':' + port + '/' + result.__data.profilePicture;
             roleMapping.find({ include: { relation: 'role' }, where: { principalId: result.__data.id } }, function (err, roleMappingResults) {
-            if (roleMappingResults.length > 0) {
-                result.__data.roleName = [];
-                roleMappingResults.forEach(function (roleMappingResult) {
-                    result.__data.roleName.push(roleMappingResult.__data.role.__data.name)
-                   
-                })
-                
-                if (index == context.result.length - 1) {
-                    next();
+                if (roleMappingResults.length > 0) {
+                    result.__data.roleName = [];
+                    roleMappingResults.forEach(function (roleMappingResult) {
+                        //add role name
+                        result.__data.roleName.push(roleMappingResult.__data.role.__data.name)
+
+                    })
+                    
+                    //when the looping has done
+                    if (index == context.result.length - 1) {
+                        next();
+                    }
                 }
-            }
-        })            
+            })
         })
     })
   
     //send password reset link when requested
     user.on('resetPasswordRequest', function (info) {
+        
+        //get config setting of host & port from config.json
         var host = (user.app && user.app.settings.host);
         var port = (user.app && user.app.settings.port);
+        
+        //set url of reset password
         var url = host + ':' + port + '/#/reset-password';
+        
+        //make an email content. This email contains url with parameter hash password, access token, email address & userId
         var html = 'Click <a href="http://' + url + '?password=' +
             info.user.password + '&token=' + info.accessToken.id + '&email=' + info.user.email + '&id=' + info.user.id + '">here</a> to reset your password';
-
+        
+        //set the options of the email     
         var options = {
             type: 'email',
             to: info.email,
@@ -321,7 +434,8 @@ module.exports = function (user) {
 
         var Email = options.mailer || this.constructor.email || loopBack.getModelByType(loopBack.Email);
         options.headers = options.headers || {};
-
+        
+        //send an email
         Email.send(options, function (err, email) {
             if (err) {
                 console.log(err)
--- server/boot/script.js
+++ server/boot/script.js
@@ -5,17 +5,26 @@ module.exports = function (app) {
     var roleMapping = app.models.RoleMapping;
     var fs = require('fs')
     var path = require('path');
+    
+    //create a relation from roleMapping to user
     roleMapping.belongsTo(user, { foreignKey: 'principalId' });
+    
+    //create a relation from roleMapping to role
     roleMapping.belongsTo(role, { foreignKey: 'roleId' });
 
     //create admin role, skip if already created
-    role.find({ name: 'admin' }, function (err, roleAdminResults) {
+    role.find(function (err, roleAdminResults) {
+        
+        //when there is no any role, then create the default admin and member user
         if (roleAdminResults.length == 0) {
+      
+            //create a admin role
             role.create({
                 name: 'admin'
             }, function (err, roleResult) {
                 if (err) return err;
-
+                
+                //create a default admin user
                 user.create({
                     username: 'admin',
                     email: 'admin@admin.com',
@@ -34,6 +43,7 @@ module.exports = function (app) {
                     if (err) {
                     }
                     else {
+                        //make admin user as an admin
                         roleResult.principals.create({
                             principalType: roleMapping.USER,
                             principalId: userResult.id
@@ -43,10 +53,13 @@ module.exports = function (app) {
                     }
                 });
             });
-
+            
+            //create a member role
             role.create({
                 name: 'member'
             }, function (err, roleResult) {
+                
+                //create a default member user
                 user.create({
                     username: 'member',
                     email: 'member@member.com',
@@ -66,6 +79,7 @@ module.exports = function (app) {
 
                     }
                     else {
+                        //make member user as a member
                         roleResult.principals.create({
                             principalType: roleMapping.USER,
                             principalId: userResult.id
